9c484aa65810eed92ffb0a85f3a10e12a6837006
cvhnilicka <cvhnilicka@wisc.edu>
2020-07-21 12:16:41 -0700

    ship now moving on camera track

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    [SerializeField] float levelLoadDelay = 3f;
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject); // dont destroy the game object that the music player is attached to
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Variables n stuff
+    [Tooltip("In ms^1")][SerializeField] float xSpeed = 9f;
+    [Tooltip("In ms^1")] [SerializeField] float ySpeed = 7f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // X Axis Calculations
+        float horizontalThrow = Input.GetAxis("Horizontal");
+        float xFrameOffset = horizontalThrow * xSpeed * Time.deltaTime; // offset this frame
+        float rawX = transform.localPosition.x + xFrameOffset;
+
+        // Y Axis calculations
+        float verticalThrow = Input.GetAxis("Vertical");
+        float yFrameOffset = verticalThrow * ySpeed * Time.deltaTime;
+        float rawY = transform.localPosition.y + yFrameOffset;
+
+        transform.localPosition = new Vector3( Mathf.Clamp(rawX, -4.25f, 4.25f), 
+            Mathf.Clamp(rawY, -2.25f, 2.25f), 
+            transform.localPosition.z);
+    }
+}
