56352f3844b362af328b3e865a900cd3c02fe718
cvhnilicka <cvhnilicka@wisc.edu>
2020-07-26 16:14:47 -0700

    tidying up a bit

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,17 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MusicPlayer : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
-
     [SerializeField] float levelLoadDelay = 3f;
-
-
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject); // dont destroy the game object that the music player is attached to
-    }
-
 
 
     // Start is called before the first frame update
@@ -28,7 +20,7 @@ public class MusicPlayer : MonoBehaviour
         
     }
 
-    private void LoadNextLevel()
+        private void LoadNextLevel()
     {
         SceneManager.LoadScene(1);
 
